82bc3dfbcdab17670663ae91e64500fecf5471f2
Anartz Mugika Ledo <mugan86@gmail.com>
2021-11-26 19:06:29 +0100

    Build in production

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { GithubUserModule } from 'github-user-widget';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,7 +10,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     GithubUserModule
   ],
   providers: [],
